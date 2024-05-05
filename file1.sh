a3f006cb92df0e0e9be02566c81b21f091f14777
Naveen-Kamagani <Naveen.Kamagani@ibm.com>
2024-05-05 16:33:16 +0530

    adding file1.sh

---
+++ file1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+create_dir(){
+  DIRECTORY=$1
+  if [ ! -d "$DIRECTORY" ]; then
+    echo "$DIRECTORY does not exist."
+    mkdir $DIRECTORY
+    echo "$DIRECTORY creation completed"
+  else
+    echo  "$DIRECTORY exists."
+  fi
+
+}
+create_file(){
+  filename=$1
+
+  if ! [ -f "$filename" ]; then
+    echo "File does not exist."
+    touch $filename
+    echo " $filename creation completed"
+  else 
+    echo "$filename exists"
+  fi
+}
+create_dir dir2/navi
+create_dir dir2/nandhu
+create_dir dir2/nani
+
+create_file dir2/navi/file1.sh
+create_file dir2/nandhu/file1.sh
+create_file dir2/nani/file1.sh
+
+
